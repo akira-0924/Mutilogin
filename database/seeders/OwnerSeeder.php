d12847b85b20e2673ca28f068db56e44026c3c03
水島アキラ <mizushima.akira@i3design.co.jp>
2024-01-31 20:51:52 +0900

    user、owner、adminの認証実装

---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('owners')->insert([
+            [
+                'name' => 'admin1',
+                'email' => 'testowner1@test.com',
+                'password' => 'password123'
+            ],
+        ]);
+    }
+}
